ac7d712d41766281dbf6fe349b0c71dc604c9902
Samim Aktar <samimaktar0773@gmail.com>
2021-09-29 10:30:32 +0530

    Complete UC 1: Emp is present or Absent

--- EmpWageCom.java
+++ EmpWageCom.java
@@ -1,5 +1,24 @@
 public class EmpWageCom{
 	public static void main(String args []){
 		System.out.println("Welcome to Employee Wage Computation Program");
+		// calling CheckEmp method
+		CheckEmp();
+		
+		
+	}
+	// Create a method for checkemp
+	public static void CheckEmp(){
+		
+		// initialize and declar a emp_is_present variable 
+		int emp_is_present=1;
+		
+		//take the random value
+		double checkEmp = Math.floor(Math.random()*10)%2;
+		
+		//check the codition emp is present or not 
+		if(checkEmp == emp_is_present)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Emplyee is Absent");
 	}
 }
